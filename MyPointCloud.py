84c0275d96a617cb8c1dd8175217321a3f7efc38
Cindy Grimm <cindy.grimm@oregonstate.edu>
2020-01-18 08:08:39 -0800

    Shifting to pymesh to read points Not storing points internally, but storing as pymesh Simplified find neigbors to just find breadth first search on bins then check point distances Only storying bins and bin ids for points, not neighbors Switched to using list_read_write for RW

--- MyPointCloud.py
+++ MyPointCloud.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python3
 
-
-from pypcd import pypcd as pypcd
-
+import pymesh
 import numpy as np
-import pickle
-from Cylinder import Cylinder
-from test_pts import Best_pts, Bad_pts
 from list_read_write import ReadWrite
-from bitarray import bitarray
 
 
 class MyPointCloud(ReadWrite):
@@ -21,24 +15,29 @@ class MyPointCloud(ReadWrite):
                     self.offsets.append([ix, iy, iz])
 
         # Set in load point cloud
-        self.pc_data = np.zeros([1,3])
+        self.pcd_data = None
+        self.pcd_data_name = ""
         self.min_pt = [1e30, 1e30, 1e30]
         self.max_pt = [-1e30, -1e30, -1e30]
 
         # Filled in by creating the bins
-        self.radius_neighbor = 0.01  # Defines neighbor radius
-        self.div = 0.005             # Defines width of bin
+        self.div = 0.005                  # Defines width of bin
         self.start_xyz = [0.0, 0.0, 0.0]  # bottom left corner of box containing points
         self.n_bins_xyz = [1, 1, 1]       # Number of bins in each direction
         self.bin_offset = [1, 1, 1]       # for mapping bin xi,yi,zi to unique number
 
         # For mapping bin xi,yi,zi to single indes
-        self.bin_ipts = []  # xi,yi,zi for each point
-        self.bin_ids = []   # bin index for each point
+        self.bin_ids = []   # bin index for each point, can recover ix, iy, iz from _bin_ipt
         self.bin_list = {}  # bins, key is bin index, list of points in bin
 
-        # Filled in by find_neighbors
-        self.neighbors = []  # List of lists of neighbors for each point
+    def pts(self):
+        return self.pcd_data.vertices
+
+    def pt(self, i):
+        return self.pcd_data.vertices[i]
+
+    def n_pts(self):
+        return len(self.pcd_data.vertices)
 
     def _bin_index_pt(self, p):
         """
@@ -60,18 +59,26 @@ class MyPointCloud(ReadWrite):
         """
         return ipt[2] + ipt[1] * self.bin_offset[0] + ipt[0] * self.bin_offset[1]
 
-    def _bin_center(self, index):
+    def bin_ipt(self, index):
         """
         The x,y,z point in real space that is the bin center
         :param index: bin index
-        :return: x,y,z
+        :return: xi,yi,zi
         """
         zi = index % self.n_bins_xyz[2]
         xy_index = int((index - zi) / self.n_bins_xyz[2])
         yi = xy_index % self.n_bins_xyz[1]
         x_index = xy_index - yi
         xi = int(x_index / self.n_bins_xyz[1])
-        ipt = [xi, yi, zi]
+        return [xi, yi, zi]
+
+    def _bin_center(self, index):
+        """
+        The x,y,z point in real space that is the bin center
+        :param index: bin index
+        :return: x,y,z
+        """
+        ipt = self.bin_ipt(index)
 
         pt = []
         for i in range(0, 3):
@@ -94,59 +101,45 @@ class MyPointCloud(ReadWrite):
             return 2
         return dist_pt + 1 - norm_align
 
-    def find_neighbors(self):
+    def find_neighbor_bins(self, pi_index, radius_search):
         """
         Search the neighboring bins for points within the given radius
-        :return: list of ids of neighbors
+        :param: pi_index: index of point
+        :param: radius_search: maximum radius to try
+        :return: list of ids of neighbor bin ids within radius_search
         """
-        self.neighbors = []
-
-        # For each point, get the points in the bins around it
-        avg_n = 0
-        for i, p in enumerate(self.pc_data):
-            ipt = self.bin_ipts[i]
-            qs = []
+        print("Find neighbors n points {0}".format(len(self.bin_list)))
+        p = self.pt(pi_index)
+        last_count = 0
+        ret_list = [self.bin_ids[pi_index]]
+        while len(ret_list) > last_count:
+            ipt = self.bin_ipt(ret_list[last_count])
             for o in self.offsets:
                 ipt_search = [ipt[i] + o[i] for i in range(0, 3)]
                 i_bin = self._bin_index_map(ipt_search)
-                if i_bin in self.bin_list:
-                    qs.extend(self.bin_list[i_bin])
-
-            ns = []
-            for qi in qs:
-                if i != qi:
-                    dist_q = self.dist(p, self.pc_data[qi])
-                    if dist_q < self.radius_neighbor:
-                        ns.append((qi, dist_q))
-            self.neighbors.append(ns)
-            avg_n += len(ns)
-        return avg_n / len(self.neighbors)
+                if i_bin not in ret_list and i_bin in self.bin_list:
+                    q = self._bin_center(i_bin)
+                    if self.dist(p, q) < radius_search:
+                        ret_list.append(i_bin)
+            last_count += 1
+        return ret_list
 
     def find_connected(self, pi_start, in_radius=0.05):
         """
         Find all the points that are within the radius AND connected (breadth first search)
         :param pi_start: Point index to start at
         :param in_radius: Maximum Euclidean distance allowed
-        :return: Dictionary of neighbors containing distances to neighbors
+        :return: Dictionary of neighbors containing distances to neighbors (id, pt, dist)
         """
-        visited = {}
-        to_visit = {pi_start: 0.0}
-        ret_list = {pi_start: 0.0}
-        p = self.pc_data[pi_start]
-        while to_visit:
-            # Get next item off the list
-            (v, d) = to_visit.popitem()
-            visited[v] = True
-            # Check its neighbors
-            for (q, dn) in self.neighbors[v]:
-                if not (q in visited) and not (q in to_visit):
-                    d_new = self.dist(p, self.pc_data[q])
-                    # if close enough, add
-                    if d_new < in_radius:
-                        to_visit[q] = d_new
-                        ret_list[q] = d_new
-                    else:
-                        visited[q] = True  # no sense in looking at this again
+        bin_id_list = self.find_neighbor_bins(pi_start, in_radius + self.div)
+        p = self.pt(pi_start)
+        ret_list = []
+        for i_bin in bin_id_list:
+            for q_id in self.bin_list[i_bin]:
+                q = self.pt(q_id)
+                d_pq = self.dist(p, q)
+                if d_pq < in_radius:
+                    ret_list.append((q_id, q, d_pq))
         return ret_list
 
     def _reorder_pts_in_bins(self):
@@ -154,27 +147,27 @@ class MyPointCloud(ReadWrite):
         Put the point that is closest to the center of the bin first in the list
         :return: None
         """
-        for k, b in self.bin_list.items():
+        for k, bl in self.bin_list.items():
             pt_center = self._bin_center(k)
             dist_to_center = []
-            for p_id in b:
-                dist_to_center.append((p_id, MyPointCloud.dist(pt_center, self.pc_data[p_id])))
+            for p_id in bl:
+                dist_to_center.append((p_id, MyPointCloud.dist(pt_center, self.pt(p_id))))
             dist_to_center.sort(key=lambda list_item: list_item[1])
 
-            for i in range(0, len(b)):
-                b[i] = dist_to_center[i][0]
+            for i in range(0, len(bl)):
+                bl[i] = dist_to_center[i][0]
 
     def create_bins(self, in_smallest_branch_width=0.01):
         """
         Make bins and put points in the bins. Aim for bins that are 1/3 radius of smallest branch width
         :param in_smallest_branch_width: Smallest branch width
-        :return:
+        :return: bin list
         """
         max_width = 0
         for i in range(0, 3):
             max_width = max(max_width, self.max_pt[i] - self.min_pt[i])
-        self.radius_neighbor = 2.0 * in_smallest_branch_width / 3.0
-        self.div = self.radius_neighbor / 2.0
+        radius_neighbor = 2.0 * in_smallest_branch_width / 3.0
+        self.div = radius_neighbor / 2.0
         # pad by one row of bins
         self.start_xyz = [self.min_pt[i] - self.div - 0.0001 for i in range(0, 3)]
         self.n_bins_xyz = [int(np.ceil((self.max_pt[i] + self.div - self.start_xyz[i]) / self.div)) for i in range(0, 3)]
@@ -182,19 +175,22 @@ class MyPointCloud(ReadWrite):
         self.bin_offset = [self.n_bins_xyz[2], self.n_bins_xyz[2] * self.n_bins_xyz[1]]
 
         # Put each point in its bin
-        self.bin_ipts = []
         self.bin_ids = []
         self.bin_list = {}
         min_ipt = [1000, 1000, 1000]
         max_ipt = [0, 0, 0]
 
-        for i, p in enumerate(self.pc_data):
+        print("Finding bins n points: {0}, div {1:0.4f}".format(self.n_pts(), self.div))
+        for i, p in enumerate(self.pts()):
+            if i % 1000 == 0:
+                if i % 10000 == 0:
+                    print("{0} ".format(i), end='')
+
             ipt = self._bin_index_pt(p)
             for j in range(0, 3):
                 min_ipt[j] = min(min_ipt[j], ipt[j])
                 max_ipt[j] = max(max_ipt[j], ipt[j])
             index = self._bin_index_map(ipt)
-            self.bin_ipts.append(ipt)
             self.bin_ids.append(index)
             try:
                 list_in_bin = self.bin_list[index]
@@ -208,7 +204,7 @@ class MyPointCloud(ReadWrite):
         n_count = 0
         max_count = 0
 
-        bins_as_bit_arrays = {}
+        # bins_as_bit_arrays = {}
         for k, pts_in_bin in self.bin_list.items():
             n_count += 1
             n_pts_in_bin_avg += len(pts_in_bin)
@@ -232,77 +228,75 @@ class MyPointCloud(ReadWrite):
 
         return self.bin_list
 
-    def write(self, file_name):
-        with open(file_name, 'wb') as fid:
-            for_dump = list(self.bin_list.items())
-            self.bin_list = for_dump
-            pickle.dump(self, fid)
-        self.fix_bin_list()
-
-    def fix_bin_list(self):
-        bin_list = {}
-        for k in self.bin_list:
-            bin_list[k[0]] = k[1]
-        self.bin_list = bin_list
-        return self
-
-    def load_point_cloud(self, file_name):
-        pcd_data = pypcd.point_cloud_from_path(file_name)
+    def load_point_cloud(self, file_name=None):
+        if file_name is None:
+            file_name = self.pcd_data_name
+        else:
+            self.pcd_data_name = file_name
+        self.pcd_data = pymesh.load_mesh(file_name)
+        """
         self.pc_data = []
-        for p in pcd_data.pc_data:
+        
+        for p in pcd_data.vertices:
             if p[1] > 1.25 or p[2] > 1.7:
                 continue
             self.pc_data.append(p)
 
         self.pc_data = np.array(self.pc_data)
+        """
 
         #  Find bounding box
         self.min_pt = [1e30, 1e30, 1e30]
         self.max_pt = [-1e30, -1e30, -1e30]
-        for p in self.pc_data:
+        for p in self.pts():
             for i in range(0, 3):
                 self.min_pt[i] = min(self.min_pt[i], p[i])
                 self.max_pt[i] = max(self.max_pt[i], p[i])
 
+    def read(self, fid):
+        self.check_header(fid)
+        self.read_class_members(fid)
+        self.pcd_data = pymesh.load_mesh(self.pcd_data_name)
 
-def load(in_fname):
-    with open(in_fname, 'rb') as fid:
-        my_pcd_load: MyPointCloud = pickle.load(fid)
-    my_pcd_load.fix_bin_list()
-    return my_pcd_load
+    def write(self, fid):
+        self.write_header(fid)
+        self.write_class_members(fid, dir(self), MyPointCloud, ["pcd_data"])
+        self.write_footer(fid)
 
 
 if __name__ == '__main__':
-    b_read = False
+    b_read = True
 
-    name_file = "point_clouds/bag_3/cloud_final.pcd"
-    #  name_file = "final_fused.pcd"
-    fname_read = "data/MyPointCloud.pickle"
+    my_pcd = MyPointCloud()
+
+    name_file = "data/point_clouds/bag_3/cloud_final.ply"
+    # name_file = "final_fused.pcd"
+    fname_rw = "data/MyPointCloud.txt"
+
     if b_read:
-        my_pcd = load("data/MyPointCloud.pickle")
+        with open(fname_rw, "r") as fid_in:
+            my_pcd.read(fid_in)
 
-        cyl = Cylinder()
-        cyl_pts = Best_pts()
-        cyl_pts.update(Bad_pts())
+        print("Reading: n bins: {0}, bin width {1:0.4f}".format(len(my_pcd.bin_list), my_pcd.div))
+        print("Point cloud width/height: ", end=" ")
+        vec = np.array(my_pcd.max_pt) - np.array(my_pcd.min_pt)
+        print(vec)
+        print(my_pcd.min_pt)
+        print(my_pcd.max_pt)
+        print("Expected bin width {0:0.4f}".format(np.linalg.norm(vec) / 500))
+        bin_count_bds = [0, 5, 15, 25, 45, 100, 1000000]
+        bin_count = [0, 0, 0, 0, 0, 6]
+        for _, b in my_pcd.bin_list.items():
+            b_len = len(b)
+            for bi, bd in enumerate(bin_count_bds[:-1]):
+                if bd <= b_len < bin_count_bds[bi+1]:
+                    bin_count[bi] += 1
 
-        for pt_id, label in cyl_pts.items():
-            radius = 0.06
-            if label.startswith("Thin"):
-                radius = 0.05
-            elif label.startswith("Trunk"):
-                radius = 0.1
-
-            pt_ids = my_pcd.find_connected(pt_id, radius)
-            cyl.set_fit_pts(pt_id, pt_ids.keys(), my_pcd.pc_data)
-            fname = "data/cyl_{0}.txt".format(pt_id)
-            with open(fname, "w") as f:
-                cyl.write(f, write_pts=True)
-
-            print("Type {0}".format(label))
-            print(cyl)
+        print("Bin counts:")
+        for bi, bd in enumerate(bin_count_bds[:-1]):
+            print("{0} - {1}, {2}".format(bd, bin_count_bds[bi+1], bin_count[bi]))
     else:
-        my_pcd = MyPointCloud()
-        my_pcd.load_point_cloud("data/" + name_file)
+        my_pcd.load_point_cloud(name_file)
         smallest_branch_width_apple = 0.06
         smallest_branch_width_cherry = 0.04
         print("Creating bins: ", end="")
@@ -312,9 +306,11 @@ if __name__ == '__main__':
         vec = np.array(my_pcd.max_pt) - np.array(my_pcd.min_pt)
         print(vec)
         print("Expected bin width {0:0.4f}".format(np.linalg.norm(vec) / 500))
-        print("Finding neighbors")
-        n_avg = my_pcd.find_neighbors()
-        print("Avg neigh {0}".format(n_avg))
-        my_pcd.write(fname_read)
 
+        with open(fname_rw, "w") as fid_out:
+            my_pcd.write(fid_out)
 
+    for pid_rand in np.random.uniform(0, 1, 40):
+        pid = int(np.floor(pid_rand * my_pcd.n_pts()))
+        ret_val = my_pcd.find_connected(pid, my_pcd.div * 10.0)
+        print(ret_val)
