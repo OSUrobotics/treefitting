Splitting out spline with cylinder and control hull Cleaning up code for generating 3D extruded cylinders and points along edge in 2D curve Fixing derivatives for cubic curves

tree_geometry/b_spline_curve.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
-from typing import Union, Any
-
-from numpy import floating
-
 """
 Resources:
 https://mathworld.wolfram.com/B-Spline.html
@@ -10,13 +6,21 @@ https://xiaoxingchen.github.io/2020/03/02/bspline_in_so3/general_matrix_represen
 """
 
 import numpy as np
+from typing import Union, Any
+from numpy import floating
 from numpy.polynomial.polynomial import polyval
 from scipy.optimize import fmin
 from scipy.integrate import quad
 
-from tree_geometry.point_lists import ControlHull
+from tree_geometry.control_hull import ControlHull
 
 
+# A b-spline curve (linear, quadratic, or cubic) defined as a sequence of control points (ControlHull)
+# - Defined as f(t) -> [x,y,z] for t in 0,# pts - dim
+# - Always a uniform knot vector
+# Supports
+#   Tacking on another control point
+#   Projecting points onto the curve (for fitting)
 class BSplineCurve(ControlHull):
     # For initializing with a name instead of a dimension
     _degree_dict = dict(
@@ -40,19 +44,31 @@ class BSplineCurve(ControlHull):
                              [-1,  3, -3, 1]]), # t^3
     }
     _derivative_dict = {
-        0: np.array([0]),                  # Shift the basis up by 1 and divide by power of t
+        0: np.array([0]),                  # Shift the basis up by 1 and multiply by power of t
         1: np.array([[-1, 1],              # -1
                      [ 0, 0]]),            #  1
         2: 1 / 2 * np.array([[-2,   2, 0],    # 2 t - 2
                              [ 2,  -4, 2],    # -4 t + 2
                              [0,    0, 0]]),  # 2 t
-        3: 1 / 6 * np.array([[-3, 0, 3, 0],
-                             [3/2, -6/2, 3/2, 0],
-                             [-1/3, 3/3, -3/3, 1/3],
-                             [0, 0, 0, 0]]),
+        3: 1 / 6 * np.array([[-3,   0,  3, 0],  # -3 t^2 + 6 t - 3
+                             [ 6, -12,  6, 0],  #  9 t^2 -12 t
+                             [-3,   9, -9, 3],  # -9 t^2 + 6 t + 3
+                             [0, 0, 0, 0]]),          # 3 t^2
+    }
+    _second_derivative_dict = {
+        0: np.array([0]),                  # Shift the basis up by 1 and divide by power of t
+        1: np.array([[0, 0],               # 0
+                     [0, 0]]),             # 0
+        2: 1 / 2 * np.array([[2, -4, 2],    # 2
+                             [0,  0, 0],    # -4
+                             [0,  0, 0]]),  # 2
+        3: 1 / 6 * np.array([[ 6, -12,   6, 0],    # -6 t + 6
+                             [-6,  18, -18, 6],    # 18 t - 12
+                             [ 0,   0,   0, 0],    # -18 t + 6
+                             [0, 0, 0, 0]]),       # 6 t
     }
 
-    def __init__(self, ctrl_pts: Union[list[np.ndarray], np.ndarray], degree: str = "quadratic") -> None:
+    def __init__(self, ctrl_pts: Union[list[np.ndarray], np.ndarray, list[list]], degree: str = "quadratic") -> None:
         """BSpline initialization
         :param ctrl_pts: control points, list of numpy array points of desired dimension
         :param degree: degree of spline, defaults to "quadratic"
@@ -68,9 +84,13 @@ class BSplineCurve(ControlHull):
         # Set the bases matrics for the given degree
         self._basis_matrix: np.ndarray = BSplineCurve._basis_matrix_dict[self._degree]
         self._deriv_matrix: np.ndarray = BSplineCurve._derivative_dict[self._degree]
+        self._second_deriv_matrix: np.ndarray = BSplineCurve._second_derivative_dict[self._degree]
 
     def degree(self):
         return self._degree
+
+    def n_segments(self):
+        return self.n_points() - self._degree
 
     @property
     def basis_matrix(self):
@@ -165,13 +185,80 @@ class BSplineCurve(ControlHull):
         """
         t_clamp = self.clamp_t(t)
         idx = np.floor(t_clamp).astype(int)
-        eval_basis_matrix = BSplineCurve.eval_basis(basis_to_use=self._deriv_matrix, t=t_clamp - idx)
+        # Evaluate with derivative matrix instead of basis matrix
+        eval_deriv_basis_matrix = BSplineCurve.eval_basis(basis_to_use=self._deriv_matrix, t=t_clamp - idx)
         if isinstance(t_clamp, float):
-            return eval_basis_matrix @ self.points_as_ndarray()[idx:idx+self.order()]
+            return eval_deriv_basis_matrix @ self.points_as_ndarray()[idx:idx+self.order()]
 
         res_vecs = np.zeros((len(t_clamp), self.dim()))
         for i_row, cp_id in enumerate(idx):
-            res_vecs[i_row, :] = eval_basis_matrix[i_row, :] @ self.points_as_ndarray()[cp_id:cp_id+self.order(), :]
+            res_vecs[i_row, :] = eval_deriv_basis_matrix[i_row, :] @ self.points_as_ndarray()[cp_id:cp_id+self.order(), :]
+        return res_vecs
+
+    def eval_second_deriv(self, t: Union[float, np.ndarray]) -> np.ndarray:
+        """Get the value of the second derivative of the spline at parameter t
+        @param t - parameter
+        @return derivative
+        """
+        t_clamp = self.clamp_t(t)
+        idx = np.floor(t_clamp).astype(int)
+        # Evaluate with second derivative matrix instead of basis matrix
+        eval_second_deriv_basis_matrix = BSplineCurve.eval_basis(basis_to_use=self._second_deriv_matrix, t=t_clamp - idx)
+        if isinstance(t_clamp, float):
+            return eval_second_deriv_basis_matrix @ self.points_as_ndarray()[idx:idx+self.order()]
+
+        res_vecs = np.zeros((len(t_clamp), self.dim()))
+        for i_row, cp_id in enumerate(idx):
+            res_vecs[i_row, :] = eval_second_deriv_basis_matrix[i_row, :] @ self.points_as_ndarray()[cp_id:cp_id+self.order(), :]
+        return res_vecs
+
+    def _norm(self, vec_deriv: np.ndarray, vec_second_deriv: np.ndarray):
+        """ Get the unit-length normal (either 2D or 3D) from the tangent
+        @param vec_deriv: Derivative
+        @param vec_second_deriv: Second derivative vector (constant for dim 2)
+        """
+        if self.dim() == 1:
+            return np.array([0.0])
+        elif self.dim() == 2:
+            vec_length = np.linalg.norm(vec_deriv)
+            if np.isclose(vec_length, 0.0):
+                return np.array([0.0, 0.0])
+            return np.array([-vec_deriv[1] / vec_length, vec_deriv[0] / vec_length])
+        elif self.dim() == 3:
+            vec_binormal = np.cross(vec_deriv, vec_second_deriv)
+            if np.isclose(np.linalg.norm(vec_second_deriv), 0.0):
+                for i in range(0, 2):
+                    if not np.isclose(vec_deriv[i], 0.0):
+                        vec_binormal[i] = -vec_deriv[(i + 1) % 3]
+                        vec_binormal[(i + 1) % 3] = vec_deriv[i]
+                        vec_binormal[(i + 2) % 3] = 0.0
+                        break
+
+            return vec_binormal / np.linalg.norm(vec_binormal)
+        raise ValueError("Curve not 1, 2, or 3 dimensions")
+
+    def eval_norm(self, t: Union[float, np.ndarray]) -> np.ndarray:
+        """Get the value of the normal of the derivative of the spline at parameter t
+        Note: Not defined for 1 dimensional curves; for 2 dimensional, just rotate the derivative
+        @param t - parameter
+        :param dir: direction of normal, 0 for left, 1 for right
+        @return normal of the curve
+        """
+        t_clamp = self.clamp_t(t)
+        idx = np.floor(t_clamp).astype(int)
+        # Evaluate with derivative matrix instead of basis matrix
+        eval_deriv_basis_matrix = BSplineCurve.eval_basis(basis_to_use=self._deriv_matrix, t=t_clamp - idx)
+        eval_second_deriv_basis_matrix = BSplineCurve.eval_basis(basis_to_use=self._second_deriv_matrix, t=t_clamp - idx)
+        if isinstance(t_clamp, float):
+            vec_deriv = eval_deriv_basis_matrix @ self.points_as_ndarray()[idx:idx+self.order()]
+            vec_second_deriv = eval_second_deriv_basis_matrix @ self.points_as_ndarray()[idx:idx+self.order()]
+            return self._norm(vec_deriv, vec_second_deriv)
+
+        res_vecs = np.zeros((len(t_clamp), self.dim()))
+        for i_row, cp_id in enumerate(idx):
+            vec_deriv = eval_deriv_basis_matrix[i_row, :] @ self.points_as_ndarray()[cp_id:cp_id+self.order(), :]
+            vec_second_deriv = eval_second_deriv_basis_matrix[i_row, :] @ self.points_as_ndarray()[cp_id:cp_id+self.order(), :]
+            res_vecs[i_row, :] = self._norm(vec_deriv, vec_second_deriv)
         return res_vecs
 
     def curve_length(self):
@@ -227,53 +314,110 @@ class BSplineCurve(ControlHull):
 
         # Standard fmin search for distance from curve
         t_min = fmin(self.get_distance_from_curve, np.array(t_start), args=(pt,), disp=False)  # limit to 10 TODO
-        if t_min[0] < 0.0 or t_min[0] > self.max_t():
-            t_min[0] = t_start  # Bail to the original t if fit went haywire
+        t_best = t_min[0]
+        if t_best < 0.0 or t_best > self.max_t():
+            t_best = t_start # Bail to the original t if fit went haywire
 
-        pt_proj = self.eval_crv(t_min[0])
-        return t_min[0], pt_proj, np.linalg.norm(pt_proj - pt)
+        pt_proj = self.eval_crv(t_best)
+        return t_best, pt_proj, np.linalg.norm(pt_proj - pt)
 
 
 if __name__ == "__main__":
     # np.set_printoptions(precision=3, suppress=True)
 
+    cv = [[1, 3], [1, 2, -1], [1, 2, 2.5, -1]]
+    checks = [[cv[0][0], cv[0][1], cv[0][1] - cv[0][0], cv[0][1] - cv[0][0]],
+              [0.5*(cv[1][0] + cv[1][1]), 0.5*(cv[1][1] + cv[1][2]), (cv[1][1] - cv[1][0]), (cv[1][2] - cv[1][1])],
+              [1.0/6.0*(cv[2][0] + cv[2][2]) + 4.0/6.0 * cv[2][1], 1.0/6.0*(cv[2][1] + cv[2][3]) + 4.0/6.0 * cv[2][2],
+                0.5*(cv[2][2] - cv[2][0]), 0.5*(cv[2][3] - cv[2][1])] ]
     for dim_check in range(1, 3):
-        for deg_check in ['linear', 'quadratic', 'cubic']:
+        for i_deg, deg_check in zip([2, 3, 4], ['linear', 'quadratic', 'cubic']):
             print(f"dimension {dim_check} degree {deg_check}")
-            cntrl_hull = []
-            for i_pt in range(0, 4):
-                cntrl_hull.append(i_pt * np.ones(dim_check))
-            crv_check = BSplineCurve(ctrl_pts=cntrl_hull, degree=deg_check)
-            pts_check = crv_check.eval_crv(np.array([0.0, 0.25, 0.5, 0.75, 0.999999999]))
 
-            crv_check.eval_crv(np.linspace(0.0, crv_check.max_t(), 20))
-            eps = 0.0001
+            # First check - start point
+            #.   Linear - start point is at first point
+            #.   Quadratic - start point is half way between first and second point
+            #.   Cubic - start point is 1/6 p0 + 4/6 p1 + 1/6 p2
+            # Second check - end point
+            #.   Linear - end point is last point
+            #.   Quadratic - end point is half way between second to last and last point
+            #.   Cubic - end point is 1/6 p1 + 4/6 p2 + 1/6 p3
+
+            # Third check - start deriv
+            #.   Linear - start deriv is end point - start point
+            #.   Quadratic - start derivative is (p1 - p0)
+            #.   Cubic - start derivative is 1/2 (p2 - p0)
+
+            # Fourth check - 3nd deriv
+            #.   Linear - end deriv is end point - start point
+            #.   Quadratic - end derivative is (p2 - p1)
+            #.   Cubic - start derivative is 1/2 (p3 - p1)
+
+            # Need n+1 control points
+            cntrl_hull = []
+            for i_pt in range(0, i_deg):
+                pt_add = np.ones(dim_check)
+                pt_add[0] *= cv[i_deg-2][i_pt]
+                if pt_add.size > 1:
+                    pt_add[1] = i_pt + 0.5
+                if pt_add.size > 2:
+                    pt_add[2] = 3.0 - pt_add[0] * 0.5
+                cntrl_hull.append(pt_add)
+
+            crv_check = BSplineCurve(ctrl_pts=cntrl_hull, degree=deg_check)
+            t_check = [0.0, 1.0]
+            pts_check = crv_check.eval_crv(np.array(t_check))
+            deriv_check = crv_check.eval_deriv(np.array(t_check))
+
+            assert np.isclose(pts_check[0][0], checks[i_deg-2][0])
+            assert np.isclose(pts_check[-1][0], checks[i_deg-2][1])
+            assert np.isclose(deriv_check[0][0], checks[i_deg-2][2])
+            assert np.isclose(deriv_check[-1][0], checks[i_deg-2][3])
+
+            # Check derivs for a sequence of t's
+            eps = 0.00001
             t_deriv_check = np.array([0.0, 0.25, 0.5, 0.75, 1.0 - 2 * eps])
             deriv_vecs = crv_check.eval_deriv(t_deriv_check)
-            for t in t_deriv_check:
-                deriv_vec = crv_check.eval_deriv(t)
-                pt_at_t_plus_eps = crv_check.eval_crv(t + eps)
-                pt_at_t = crv_check.eval_crv(t)
+            for tc in t_deriv_check:
+                deriv_vec = crv_check.eval_deriv(tc)
+                pt_at_t = crv_check.eval_crv(tc)
+                pt_at_t_plus_eps = crv_check.eval_crv(tc + eps)
                 deriv_check_vec = (pt_at_t_plus_eps - pt_at_t) / eps
-                assert np.allclose(deriv_vec, deriv_check_vec)
-            pt_mid = crv_check.eval_crv(0.4)
-            pt_mid_next = crv_check.eval_crv(0.41)
-            vec_check = (pt_mid_next - pt_mid) / 0.01
-            mid_vec = crv_check.eval_deriv(0.4)
+                assert np.allclose(deriv_vec, deriv_check_vec, atol=10.0 * eps)
 
-            assert np.isclose(vec_check, mid_vec).all()
+                deriv_vec_next = crv_check.eval_deriv(tc + eps)
+                second_deriv_check_vec = (deriv_vec_next - deriv_vec) / eps
+                second_deriv_vec = crv_check.eval_second_deriv(tc)
+                assert np.allclose(second_deriv_vec, second_deriv_check_vec, atol=10.0 * eps)
+
+                if dim_check > 1:
+                    vec_norm = crv_check.eval_norm(tc)
+                    dot_prod = np.dot(vec_norm, deriv_vec)
+                    assert np.isclose(dot_prod, 0.0, eps)
 
             crv_len = crv_check.curve_length()
-            assert crv_check.curve_length() <= crv_check.hull_length()
+            crv_len_check = 0.0
+            pts_along_crv = crv_check.eval_crv(np.linspace(0, 1.0, 100))
+            for ind_pt in range(0, pts_along_crv.shape[0]-1):
+                pt = pts_along_crv[ind_pt, :]
+                pt_next = pts_along_crv[ind_pt+1, :]
+                crv_len_check += np.linalg.norm(pt_next - pt)
+            crv_hull_len = crv_check.hull_length()
+            assert crv_len <= crv_hull_len + eps
+            assert np.isclose(crv_len_check, crv_len, atol=0.0001)
 
-            res_project_check = crv_check.project_to_curve(pt_mid)
+            t_mid = 0.5
+            pt_mid = crv_check.eval_crv(t_mid)
+            vec_norm = crv_check.eval_norm(t_mid)
+
+            # Go off the curve along the normal and projecting back onto the curve should give you the same point
+            if dim_check > 1:
+                res_project_check = crv_check.project_to_curve(pt_mid + vec_norm * 0.01)
+                assert np.isclose(res_project_check[0], 0.5, atol=0.01)
+                assert np.isclose(pt_mid[0], res_project_check[1][0], atol=0.01)
+                assert np.isclose(0.01, res_project_check[2], atol=0.01)
 
             assert crv_check.degree_name() == deg_check
-
-            print(f" pt mid {pt_mid} res t {res_project_check[0]}, res p {res_project_check[1]} {np.linalg.norm(res_project_check[1] - pt_mid)}")
-            print(f"   Deriv {deriv_vecs[0]}")
-            assert np.isclose(pt_mid, res_project_check[1], atol=0.01).all()
-            assert np.isclose(a=0.4, b=res_project_check[0], atol=0.01)
 
         print("\n")
 

tree_geometry/b_spline_cyl.py
@@ -3,132 +3,70 @@
 b_spline_cyl.py
 Author: Luke Strohbehn
 """
+
 import numpy as np
+from typing import Union
+from tree_geometry.b_spline_curve import BSplineCurve
 
-from b_spline_curve import BSplineCurve
 
+# Add a radius to a BSpline curve
+#  If points are 3D, then, supports generating a mesh
+#. Radius is a quadratic b spline with one segment (3 points)
+class BSplineCyl(BSplineCurve):
+    def __init__(self, ctrl_pts: Union[list[np.ndarray], np.ndarray, list[list]], degree: str = "quadratic", radii: Union[float, list[float]] = 1.0) -> None:
+        """BSpline with radii initialization
+        :param ctrl_pts: control points, list of numpy array points of desired dimension
+        :param degree: degree of spline, defaults to "quadratic"
+        :param radii: radii, either a single radii value for the whole curve or a list of radii values
+        """
+        super().__init__(ctrl_pts=ctrl_pts, degree=degree)
 
-class BSplineCyl2D(BSplineCurve):
-    def __init__(self, points, degree):
-        ""
-        super().__init__()
+        # The spline curve for the radii
+        radii_pts = [[1.0], [1.0], [1.0]]
 
-        # Drawing/mesh creation parameters
-        self.n_along = 10
-        self.n_around = 64
-        self.vertex_locs = np.zeros((self.n_along, self.n_around, 3))
+        if isinstance(radii, float):
+            for ind in range(0, 3):
+                radii_pts[ind][0] = radii
+        elif len(radii) == 2:
+            radii_pts[0][0] = radii[0]
+            radii_pts[1][0] = 0.5 * (radii[0] + radii[1])
+            radii_pts[2][0] = radii[1]
+        else:
+            # This should work for 1, 3, and > 3
+            radii_pts[0][0] = radii[0]
+            radii_pts[1][0] = radii[int(len(radii) / 2)]
+            radii_pts[2][0] = radii[-1]
 
-        # Radii
-        self.r0: float = 0.0
-        self.r1: float = 0.0
-        self.r2: float = 0.0
-        return
+        # Quadratic, 1d curve
+        self.radii_crv = BSplineCurve(radii_pts, "quadratic")
 
     def radius(self, t):
-        """Return radius at a point t along the spline"""
-        return
+        """Return radius at a point t along the spline
+        @param t - the t value in 0, max_t"""
+        t_radii = t / self.max_t()
+        return self.radii_crv.eval_crv(t_radii)[0]
 
-    def norm_axis(self, t: float, dir: int):
-        """
-        Get the normal vector to the curve at parameter t 
-        :param t: parameter
-        :param dir: direction of normal, 0 for left, 1 for right
-        :return: normal vector
-        """
-        vec_tang = self.tangent_axis(t)
-        vec_length = np.sqrt(vec_tang[0] * vec_tang[0] + vec_tang[1] * vec_tang[1])
-        if dir == 0:
-            return np.array([-vec_tang[1] / vec_length, vec_tang[0] / vec_length])
-        elif dir == 1:
-            return np.array([vec_tang[1] / vec_length, -vec_tang[0] / vec_length])
-        else:
-            raise ValueError("Invalid direction for normal")
-    
-    def edge_pts(self, t):
+    def edge_pts(self, t, perc_in_out=1.0):
         """ 
         Return the left and right edge of the tube as points
+        If 2D, returns points along edge (+- norm * radius)
+        If 3D, returns points along the bi-norm as well,
         :param t: parameter
+        :param perc_in_out: percentage of radius in_out (1.0 gives on edge)
         :return: 2d pts, left and right edge
         """
-        pt = self.pt_axis(t)
-        vec = self.tangent_axis(t)
-        vec_step = self.radius(t) * vec / np.sqrt(vec[0] * vec[0] + vec[1] * vec[1])
-        left_pt = [pt[0] - vec_step[1], pt[1] + vec_step[0]]
-        right_pt = [pt[0] + vec_step[1], pt[1] - vec_step[0]]
-        return left_pt, right_pt
+        pt = self.eval_crv(t)
+        vec = self.eval_norm(t)
+        radius = perc_in_out * self.radius(t)
 
-    def edge_offset_pt(self, t, perc_in_out, direction):
-        """ Go in/out of the edge point a given percentage
-        @param t - t value along the curve (in range 0, 1)
-        @param perc_in_out - if 1, get point on edge. If 0.5, get halfway to centerline. If 2.0 get 2 width
-        @param direction - 'Left' is the left direction, 'Right' is the right direction
-        @return numpy array x,y """
-        pt_edge = self.pt_axis(t)
-        vec_norm = self.norm_axis(t, direction)
-        return pt_edge + vec_norm * (perc_in_out * self.radius(t))
+        return pt + vec * radius, pt - vec * radius
 
-    def binormal_axis(self, t):
-        """ Return the bi-normal vec, cross product of first and second derivative
-        @param t in 0, 1
-        @return 3d vec"""
-        vec_tang = self.tangent_axis(t)
-        vec_tang = vec_tang / np.linalg.norm(vec_tang)
-        vec_second_deriv = np.array([2 * (self.pt0[i] - 2.0 * self.pt1[i] + self.pt2[i]) for i in range(0, 3)])
 
-        vec_binormal = np.cross(vec_tang, vec_second_deriv)
-        if np.isclose(np.linalg.norm(vec_second_deriv), 0.0):
-            for i in range(0, 2):
-                if not np.isclose(vec_tang[i], 0.0):
-                    vec_binormal[i] = -vec_tang[(i+1)%3]
-                    vec_binormal[(i+1)%3] = vec_tang[i]
-                    vec_binormal[(i+2)%3] = 0.0
-                    break
+if __name__ == "__main__":
 
-        return vec_binormal / np.linalg.norm(vec_binormal)
-    
-    def _calc_radii(self):
-        """ Calculate the radii along the branch
-        @return a numpy array of radii"""
-        return np.linspace(self.start_radii, self.end_radii, self.n_along)
-        
-    def _calc_cyl_vertices(self) -> None:
-        """Calculate the cylinder vertices"""
-        pt = np.ones(shape=(4,))
-        radii = self._calc_radii()
+    # Need n+1 control points
+    cntrl_hull = [[1, 1], [2, 1], [3, -1]]
+    crv1 = BSplineCyl(ctrl_pts=cntrl_hull, degree="quadratic", radii=0.1)
+    crv2 = BSplineCyl(ctrl_pts=cntrl_hull, degree="quadratic", radii=[0.1, 0.3])
+    crv3 = BSplineCyl(ctrl_pts=cntrl_hull, degree="quadratic", radii=[0.1, 0.3, 0.6])
 
-        for it, t in enumerate(np.linspace(0, 1.0, self.n_along)):
-            mat = self.frenet_frame(t)
-            pt[0] = 0
-            pt[1] = 0
-            pt[2] = 0
-            for itheta, theta in enumerate(np.linspace(0, np.pi * 2.0, self.n_around, endpoint=False)):
-                pt[0] = np.cos(theta) * radii[it]
-                pt[1] = np.sin(theta) * radii[it]
-                pt[2] = 0
-                pt_on_crv = mat @ pt
-
-                self.vertex_locs[it, itheta, :] = pt_on_crv[0:3].transpose()
-        return
-
-    def make_mesh(self):
-        """ Make a 3D generalized cylinder """
-        return self._calc_cyl_vertices()
-
-    def write_mesh(self, fname):
-        """Write out an obj file with the appropriate geometry
-        @param fname - file name (should end in .obj"""
-        with open(fname, "w") as fp:
-            fp.write(f"# Branch\n")
-            for it in range(0, self.n_along):
-                for ir in range(0, self.n_around):
-                    fp.write(f"v ")
-                    fp.write(" ".join(["{:.6}"] * 3).format(*self.vertex_locs[it, ir, :]))
-                    fp.write(f"\n")
-            for it in range(0, self.n_along - 1):
-                i_curr = it * self.n_around + 1
-                i_next = (it+1) * self.n_around + 1
-                for ir in range(0, self.n_around):
-                    ir_next = (ir + 1) % self.n_around
-                    fp.write(f"f {i_curr + ir} {i_next + ir_next} {i_curr + ir_next} \n")
-                    fp.write(f"f {i_curr + ir} {i_next + ir} {i_next + ir_next} \n")
-        return

tree_geometry/point_lists.py
@@ -5,6 +5,17 @@ from copy import deepcopy
 from tree_geometry.line_segs import LineSeg
 from typing import Union
 
+
+# PointList - A list of points (either 2d or 3d)
+#   Serves as the basis for ControlHull and PointListWithTs
+# PointListWithTs - assign t values to each point
+#   - monotonically increasing from 0 to 1
+#   - Initially set by length of segments
+#
+# Both classes support
+#  - adding a new point
+#  - converting the points to a numpy array of size dim() X number of points
+#
 class PointList:
     def __init__(self, initial_points: Union[np.ndarray, list[np.ndarray], list[list]]):
         """ A list of ordered points, initialized with either a list of points or an ndarray of points
@@ -85,94 +96,6 @@ class PointList:
             for i_d in range(0, self.dim()):
                 assert np.isclose(self._points[i][i_d], self._points_as_ndarray[i, i_d])
         return True
-
-
-class ControlHull(PointList):
-    def __init__(self, initial_points: Union[np.ndarray, list[np.ndarray], list[list]]):
-        """ A control hull has to have at least two points
-        @param initial_points: list of points, each point is a list of 2,3, etc dims
-           OR if point is an nxdim array, make a list of n points"""
-
-        # Keep these three forms of the points
-        # - a list of numpy arrays of dimension dim
-        # - the points as an n points x dim numpy array
-        # - LineSeg2D for each edge
-
-        self._polylines = None
-
-        super().__init__(initial_points)
-
-        self._set_polylines()
-
-    def __deepcopy__(self, memo):
-        """Deep copy constructor for ControlHull """
-        cls = self.__class__
-        result = cls.__new__(cls)
-        memo[id(self)] = result
-        # Will end up calling set_points, which does a deep copy
-        return ControlHull(self._points)
-
-    def polylines(self):
-        """ If there are n points, then there are n-1 LineSeg2D's """
-        return self._polylines
-
-    def _set_polylines(self):
-        """ Assumes the points are set and just does the line segs"""
-        self._polylines = [LineSeg(self._points[i], self._points[i + 1]) for i in range(len(self._points) - 1)]
-
-    def set_points(self, new_pts: Union[np.ndarray, list[np.ndarray], list[list]]):
-        """ Set the control null from a list of numpy arrays (or list of lists)
-        Note: This does a deep copy, not a shallow one
-        @param new_pts: list of points, each point is a list of 2,3, etc dims
-           OR if point is an nxdim array, make a list of n points"""
-        super().set_points(new_pts)
-
-        if self.n_points() < 2:
-            raise ValueError("ControlHull: Need at least two points, got {self.n_points()}")
-
-        self._set_polylines()
-
-    def add_point(self, pt: Union[np.ndarray, list]):
-        """ add the point to the list, along with a new polyline
-        @param pt: the point to add"""
-        super().add_point(pt)
-        self._polylines.append(LineSeg(self._points[-2], self._points[-1]))
-
-    def hull_length(self):
-        """ Length of hull
-        @return sum of lengths of line segments"""
-        seg_lengths = [line_i.line_length() for line_i in self._polylines]
-        return np.sum(np.array(seg_lengths))
-
-    def project_on_hull(self, point):
-        dist = 1e30
-        min_t = 0.0
-        min_seg = -1
-        min_proj = None
-        for i, line_seg in enumerate(self.polylines()):
-            pt_proj, t = line_seg.projection(point)
-            d = np.sqrt(np.sum((pt_proj - point) ** 2))
-            if d < dist:
-                min_proj = pt_proj
-                min_seg = i
-                min_t = t
-                dist = d
-        return min_t, min_proj, min_seg
-
-    def internal_check(self):
-        """ Check that all the data lines up"""
-
-        assert self._points_as_ndarray.shape[0] == len(self._polylines) + 1
-        for i in range(0, self.n_points() - 2):
-            for d in range(0, self.dim()):
-                assert np.isclose(self._polylines[i + 1].p1[d], self._polylines[i].p2[d])
-
-        for i in range(0, self.n_points() - 1):
-            for d in range(0, self.dim()):
-                assert np.isclose(self._polylines[i].p1[d], self._points[i][d])
-                assert np.isclose(self._polylines[i].p2[d], self._points[i+1][d])
-
-        return super().internal_check()
 
 
 class PointListWithTs(PointList):
@@ -295,30 +218,6 @@ if __name__ == "__main__":
     assert pt_v3.dim() == 2
     assert pt_v3.n_points() == pt_v1.n_points()
     assert pt_v3.internal_check()
-
-    # check control hull
-    control_hull = ControlHull(np.array([[0, 0], [1, 0], [1, 1], [1, 0]]))
-    assert control_hull.n_points() == 4
-    assert control_hull.dim() == 2
-    assert control_hull.hull_length() == 3.0
-    assert control_hull.internal_check()
-
-    # Add point
-    control_hull.add_point([0.5, 0.5])
-    res1 = control_hull.project_on_hull(point=np.array([0.5, -0.25]))
-    assert np.isclose(res1[0], 0.5)
-    assert np.isclose(res1[1][0], 0.5)
-    assert np.isclose(res1[1][1], 0.0)
-    assert res1[2] == 0
-    assert control_hull.internal_check()
-
-    control_hull.add_point([0.5, 0.5])
-    res2 = control_hull.project_on_hull(point=np.array([0.6, 0.6]))
-    assert (np.isclose(res2[0], 1.0))
-    assert (np.isclose(res2[1][0], 0.5))
-    assert (np.isclose(res2[1][1], 0.5))
-    assert (res2[2] == 3)
-    assert control_hull.internal_check()
 
     pts_with_ts = PointListWithTs(np.array([[0, 0], [1, 0], [1, 1], [1, 0]]))
     assert np.isclose(pts_with_ts.ts[0], 0.0)