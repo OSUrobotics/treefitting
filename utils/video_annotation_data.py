1f194d1eaf7f62ae3844e9bb4e4601a104f6539d
cindygr <cindy.grimm@oregonstate.edu>
2025-06-17 08:58:30 -0700

    Getting blueberries for main machine

--- utils/video_annotation_data.py
+++ utils/video_annotation_data.py
@@ -597,12 +597,12 @@ if __name__ == '__main__':
 
     b_draw_debug = False
     b_rename_files = False
-    b_make_edge_images = False
+    b_make_edge_images = True
     b_copy_tree = False
-    b_copy_blueberry = False
+    b_copy_blueberry = True
     b_make_pips = False
     b_add_tracks = False
-    b_redo_fit = True
+    b_redo_fit = False
 
     if b_draw_debug:
         tree_name = "bush_8_west"
@@ -625,14 +625,6 @@ if __name__ == '__main__':
             im_change = im[0:-len(im_name)] + im_name[0:3] + im_name[4:]
             print(f"  {im_name} {im_change}")
             copyfile(im, im_change)
-
-    if b_make_edge_images:
-        tree_name = "bush_8_west"
-        va_name = dest_path + tree_name + "/video_annot.json"
-        with open(va_name, "r") as f:
-            my_dict = json.load(f)
-            va = VideoAnnotationData.read_json(my_dict)
-            va.create_edge_images()
 
     if b_copy_tree:
         dest_path = path_start + "data/EnvyTree/"
@@ -680,6 +672,14 @@ if __name__ == '__main__':
         with open(va_fname, "r") as f:
             my_dict = json.load(f)
             va_back = VideoAnnotationData.read_json(my_dict)
+
+    if b_make_edge_images:
+        tree_name = "bush_1_east"
+        va_name = dest_path + tree_name + "/video_annot.json"
+        with open(va_name, "r") as f:
+            my_dict = json.load(f)
+            va = VideoAnnotationData.read_json(my_dict)
+            va.create_edge_images()
 
     if b_make_pips:
         annot_name = 'first_tree_annot'
